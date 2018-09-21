c294624f61eb4639e1437f359bd851291fe814d8
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-09-21 10:04:23 +0200

    Update VehicleRestController.kt

--- src/main/kotlin/local/example/demo/controller/VehicleRestController.kt
+++ src/main/kotlin/local/example/demo/controller/VehicleRestController.kt
@@ -18,11 +18,89 @@
 
 package local.example.demo.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import local.example.demo.assembler.VehicleResourceAssembler
+import local.example.demo.exception.VehicleNotFoundException
+import local.example.demo.model.Vehicle
+import local.example.demo.repository.VehicleRepository
+import org.springframework.hateoas.Resource
+import org.springframework.hateoas.Resources
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo
+import org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URI
+import java.net.URISyntaxException
 
 @RestController
 @RequestMapping("/api/vehicles")
-class VehicleRestController {
+class VehicleRestController internal constructor (
+        private val vehicleRepository: VehicleRepository,
+        private val vehicleResourceAssembler: VehicleResourceAssembler
+) {
 
+    @PostMapping
+    @Throws(URISyntaxException::class)
+    internal fun create(@RequestBody vehicle: Vehicle): ResponseEntity<Resource<Vehicle>> {
+        val resource = vehicleResourceAssembler.toResource(vehicleRepository.saveAndFlush(vehicle))
+        return ResponseEntity.created(URI(resource.id.expand().href)).body(resource)
+    }
+
+    @GetMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun read(@PathVariable id: Long?): Resource<Vehicle> {
+        val vehicle = vehicleRepository.findById(id!!)
+                .orElseThrow { VehicleNotFoundException(id) }
+        return vehicleResourceAssembler.toResource(vehicle)
+    }
+
+    @GetMapping
+    @Throws(URISyntaxException::class)
+    internal fun readAll(): Resources<Resource<Vehicle>> {
+        val vehicles = vehicleRepository.findAll()
+                .map(vehicleResourceAssembler::toResource).toList()
+        return Resources(vehicles,
+                linkTo(methodOn(VehicleRestController::class.java)
+                        .readAll()).withSelfRel())
+    }
+
+    @PutMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun update(@RequestBody vehicle: Vehicle, @PathVariable id: Long?): ResponseEntity<*> {
+        val updated = vehicleRepository.findById(id!!)
+                .map { temp ->
+                    temp.brand = vehicle.brand
+                    temp.name = vehicle.name
+                    vehicleRepository.saveAndFlush(temp)
+                }
+                .orElseGet {
+                    vehicleRepository.saveAndFlush(vehicle)
+                }
+        val resource = vehicleResourceAssembler.toResource(updated)
+        return ResponseEntity.created(URI(resource.id.expand().href)).body(resource)
+    }
+
+    @PatchMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun partialUpdate(@RequestBody vehicle: Vehicle, @PathVariable id: Long?): ResponseEntity<*> {
+        val updated = vehicleRepository.findById(id!!)
+                .map { temp ->
+                    if (!vehicle.brand.isNullOrBlank()) temp.brand = vehicle.brand
+                    if (!vehicle.name.isNullOrBlank()) temp.name = vehicle.name
+                    vehicleRepository.saveAndFlush(temp)
+                }
+                .orElseGet {
+                    vehicleRepository.saveAndFlush(vehicle)
+                }
+        val resource = vehicleResourceAssembler.toResource(updated)
+        return ResponseEntity.created(URI(resource.id.expand().href)).body(resource)
+    }
+
+    @DeleteMapping("/{id}")
+    @Throws(URISyntaxException::class)
+    internal fun cancel(@PathVariable id: Long?): ResponseEntity<*> {
+        if (id != null) {
+            vehicleRepository.deleteById(id)
+        }
+        return ResponseEntity.noContent().build<Any>()
+    }
 }
