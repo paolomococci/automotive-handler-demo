ad0d40f7d0d2dcf2fafc46d4d67c7afcac8a0e3a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2018-10-24 21:04:17 +0200

    Update VehicleRepository.kt

--- src/main/kotlin/local/example/demo/repository/VehicleRepository.kt
+++ src/main/kotlin/local/example/demo/repository/VehicleRepository.kt
@@ -19,8 +19,13 @@
 package local.example.demo.repository
 
 import local.example.demo.model.Vehicle
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 @RepositoryRestResource
-interface VehicleRepository : PagingAndSortingRepository<Vehicle, Long>
+interface VehicleRepository : PagingAndSortingRepository<Vehicle, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM VEHICLE WHERE MODEL LIKE ?1%")
+    fun searchByModel(@Param("model") model: String?): List<Vehicle>
+}
